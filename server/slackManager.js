7c2168eaac3b3975ebdcb294b7f8eeebd21f6213
morpheous <cerbus.collin@gmail.com>
2018-03-31 18:36:38 -0700

    ***

--- server/slackManager.js
+++ server/slackManager.js
@@ -48,8 +48,10 @@ function INITIALIZE() {
 		try {
 			bot = await new Slack( { wToken } );
 			discordBot = new Eris( discordCreds.token );
-			await discordBot.connect();			
-			resolve();
+			await discordBot.connect();
+			setTimeout( function() {
+				resolve();
+			} , 2000 );
 		}
 		catch( error ) { console.log( error ); reject( error ); }
 	});
