7c9960ae625e33e5c83ad3b7f4eb36e463e83789
ceberous <cerbus.collin@gmail.com>
2018-09-07 20:27:19 -0400

    35

--- server/express/app.js
+++ server/express/app.js
@@ -110,54 +110,54 @@ app.get( "/latest" , async function( req , res , next ) {
 	// });
 
 	latest_video_path = latest_video_path.split( "-" );
-	var filePath = path.join( __dirname , "../../RECORDS" , latest_video_path[ 0 ] , latest_video_path[ 1 ] );
+	var filePath = path.join( __dirname , "../../RECORDS" , latest_video_path[ 0 ] , latest_video_path[ 1 ] , "video.mp4" );
 	console.log( "Recieved File Path === " );
 	console.log( filePath );
 
-	res.json( { "testing" : filePath } );
+	//res.json( { "testing" : filePath } );
 
-	// fs.stat( filePath , function(err, stats) {
-	// 	if ( err ) {
-	// 		if ( err.code === 'ENOENT' ) {
-	// 			// 404 Error if file not found
-	// 			//return res.sendStatus(404);
-	// 			res.end( err ); // I added this
-	// 		}
-	// 		res.end( err );
-	// 	}
+	fs.stat( filePath , function(err, stats) {
+		if ( err ) {
+			if ( err.code === 'ENOENT' ) {
+				// 404 Error if file not found
+				//return res.sendStatus(404);
+				res.end( err ); // I added this
+			}
+			res.end( err );
+		}
 
-	// 	var range = req.headers.range;
+		var range = req.headers.range;
 
-	// 	if ( !range ) {
-	// 		// 416 Wrong range
-	// 		//return res.sendStatus(416);
-	// 		console.log('Err: It seems like someone tried to download the video.');
-	// 		res.end( err );
-	// 	}
-	// 	else{
-	// 		var positions   = range.replace(/bytes=/, "").split("-");
-	// 		var start       = parseInt(positions[0], 10);
-	// 		var total       = stats.size;
-	// 		var end         = positions[1] ? parseInt(positions[1], 10) : total - 1;
-	// 		var chunksize   = (end - start) + 1;
+		if ( !range ) {
+			// 416 Wrong range
+			//return res.sendStatus(416);
+			console.log('Err: It seems like someone tried to download the video.');
+			res.end( err );
+		}
+		else{
+			var positions   = range.replace(/bytes=/, "").split("-");
+			var start       = parseInt(positions[0], 10);
+			var total       = stats.size;
+			var end         = positions[1] ? parseInt(positions[1], 10) : total - 1;
+			var chunksize   = (end - start) + 1;
 
-	// 		res.writeHead( 206, {
-	// 			"Content-Range": "bytes " + start + "-" + end + "/" + total ,
-	// 			"Accept-Ranges": "bytes" ,
-	// 			"Content-Length": chunksize ,
-	// 			"Content-Type": "video/mp4"
-	// 		});
+			res.writeHead( 206, {
+				"Content-Range": "bytes " + start + "-" + end + "/" + total ,
+				"Accept-Ranges": "bytes" ,
+				"Content-Length": chunksize ,
+				"Content-Type": "video/mp4"
+			});
 
-	// 		var stream = fs.createReadStream( filePath , {
-	// 			start: start,
-	// 			end: end
-	// 		}).on("open", function() {
-	// 			stream.pipe(res);
-	// 		}).on("error", function(err) {
-	// 			res.end(err);
-	// 		});
-	// 	}
-	// });
+			var stream = fs.createReadStream( filePath , {
+				start: start,
+				end: end
+			}).on("open", function() {
+				stream.pipe(res);
+			}).on("error", function(err) {
+				res.end(err);
+			});
+		}
+	});
 
 });
 
